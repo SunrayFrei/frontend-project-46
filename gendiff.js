d8e263fb9df83a2563589534368946613141fa96
victotia tretiak <120196326+SunrayFrei@users.noreply.github.com>
2025-03-20 13:48:23 +0300

    second step of the project

--- gendiff.js
+++ gendiff.js
@@ -1,10 +1,16 @@
 #!/usr/bin/env node
- 
-import program from 'commander';
- 
+
+import { program } from 'commander';
+
 program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference.')
+    .version('1.0.0')
+    .options('-f, --format <type>', 'output format')
+    .arguments('<filepath1> <filepath2>')
+    .action((path1, path2) => {
+        console.log(fs.readFileSync(path1, 'utf8'));
+        console.log(fs.readFileSync(path2, 'utf8'));
+});
 
 program.parse();
